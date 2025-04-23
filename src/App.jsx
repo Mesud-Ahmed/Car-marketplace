4b488f16f1be27edafd8d62fe31a3deec7510730
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-04-23 06:30:19 +0300

    install and configure clerk authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
-    <>
-      <h1 className='text-3xl '>hello world</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
