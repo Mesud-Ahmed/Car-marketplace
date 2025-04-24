1f4ed9a11fc1e9dff086df372733ef8d47ea96a5
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-04-24 18:15:05 +0300

    setup a backend using drizzle orm and neon

---
+++ drizzle.config.js
@@ -0,0 +1,7 @@
+export default{
+    schema:'./configs/schema.js',
+    dialect:'postgresql',
+    dbCredentials:{
+        url:'postgresql://Car-market_owner:npg_v3KH1oWYqIDf@ep-bold-art-a4nh001s-pooler.us-east-1.aws.neon.tech/Car-market?sslmode=require',
+    }
+}
