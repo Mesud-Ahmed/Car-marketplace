16d7921207681867c0e25029762c495d98e355ab
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-04-26 07:16:50 +0300

    implement db query to show the top cars

--- next-js-car-marketplace/src/components/MostSearched.jsx
+++ next-js-car-marketplace/src/components/MostSearched.jsx
@@ -1,6 +1,9 @@
-import { Gauge, Fuel, Settings, ArrowRight } from "lucide-react";
-import corola from "../../assets/Corrolla.png";
-import ford from "../../assets/Ford Transit.png";
+import { ArrowRight } from "lucide-react";
+import { Carlisting } from "../../configs/schema";
+import Image from "next/image";
+import { MdSpeed } from "react-icons/md";
+import { BsFuelPump } from "react-icons/bs";
+import { FaCogs } from "react-icons/fa";
 import {
   Carousel,
   CarouselContent,
@@ -8,62 +11,27 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import Image from "next/image";
+import { db } from "../../configs";
+import { sql } from "drizzle-orm";
 
-function MostSearched() {
-  const cars = [
-    {
-      model: "Ford Transit – 2021",
-      specs: {
-        engine: "4.0L PowerPulse Momentum",
-        mileage: "25000 Miles",
-        fuel: "Diesel",
-        transmission: "Manual",
-      },
-      price: "$22,000",
-      image: ford,
-    },
-    {
-      model: "Corolla – 2022",
-      specs: {
-        engine: "Electric, Dual Motor",
-        mileage: "15000 Miles",
-        fuel: "Electric",
-        transmission: "Autopilot",
-      },
-      price: "$45,000",
-      image: corola,
-    },
-    {
-      model: "Ford Transit – 2021",
-      specs: {
-        engine: "4.0L PowerPulse Momentum",
-        mileage: "25000 Miles",
-        fuel: "Diesel",
-        transmission: "Manual",
-      },
-      price: "$22,000",
-      image: ford,
-    },
-    {
-      model: "Corolla – 2022",
-      specs: {
-        engine: "Electric, Dual Motor",
-        mileage: "15000 Miles",
-        fuel: "Electric",
-        transmission: "Autopilot",
-      },
-      price: "$45,000",
-      image: corola,
-    },
-  ];
+async function MostSearched() {
+  const cars = await db
+    .select()
+    .from(Carlisting)
+    .where(
+      sql`CAST(REPLACE(${Carlisting.sellingPrice}, ',', '') AS INTEGER) > 6000000`
+    )
+    .limit(10);
 
   return (
     <section className="py-16 bg-white">
       <h3 className="text-3xl font-bold text-gray-800 text-center">
-        The Most Searched Cars
+        Top Luxury Cars
       </h3>
-      <div className="mt-8 max-w-6xl  mx-auto px-12 md:px-4">
+      <h2 className="text-xl  text-gray-500 text-center py-4">
+        Discover some of the Finest Selection of our High-End Vehicles
+      </h2>
+      <div className="mt-8 max-w-6xl mx-auto px-12 md:px-4">
         <Carousel
           opts={{
             align: "start",
@@ -74,36 +42,42 @@ function MostSearched() {
           <CarouselContent>
             {cars.map((car, index) => (
               <CarouselItem
-                key={`${car.model}-${index}`}
+                key={`${car.id}-${index}`}
                 className="basis-full md:basis-1/2 lg:basis-1/3 px-2"
               >
                 <div className="bg-[#050B20] rounded-lg shadow-md w-full text-white">
                   <Image
-                    src={car.image}
-                    alt={car.model}
+                    src={car.images[0]}
+                    alt={`${car.make} ${car.model}`}
+                    width={400}
+                    height={160}
                     className="w-full h-40 md:object-cover rounded-t-lg"
                   />
+
                   <div className="p-4">
-                    <h4 className="font-bold text-lg">{car.model}</h4>
-                    <p className="text-sm mt-1">{car.specs.engine}</p>
+                    <h4 className="font-bold text-lg">
+                      {car.make} {car.model}
+                    </h4>
+                    <p className="text-sm mt-1">{car.engineSize}</p>
                     <div className="flex items-center gap-4 mt-2 text-sm">
                       <div className="flex items-center gap-1">
-                        <Gauge className="h-4 w-4" />
-                        <span>{car.specs.mileage}</span>
+                        <MdSpeed className="h-4 w-4" />
+                        <span>{car.mileage} Miles</span>
                       </div>
                       <div className="flex items-center gap-1">
-                        <Fuel className="h-4 w-4" />
-                        <span>{car.specs.fuel}</span>
+                        <BsFuelPump className="h-4 w-4" />
+                        <span>{car.type}</span>
                       </div>
+
                       <div className="flex items-center gap-1">
-                        <Settings className="h-4 w-4" />
-                        <span>{car.specs.transmission}</span>
+                        <FaCogs className="h-4 w-4" />
+                        <span>{car.transmission}</span>
                       </div>
                     </div>
                     <div className="flex justify-between items-center mt-4">
-                      <p className="font-bold text-2xl">{car.price}</p>
+                      <p className="font-bold text-2xl">${car.sellingPrice}</p>
                       <a
-                        href="#"
+                        href={`/listings/${car.id}`}
                         className="flex items-center text-sm hover:underline"
                       >
                         View Details <ArrowRight className="ml-1 h-4 w-4" />
