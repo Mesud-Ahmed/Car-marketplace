54ce8c37c2eaed3703258eeb6f2f4269e9725fe5
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-04-27 06:31:28 +0300

    extract the car item rendering logic in a separate CarItem component

---
+++ next-js-car-marketplace/src/components/CarItem.jsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import { MdSpeed } from "react-icons/md";
+import { BsFuelPump } from "react-icons/bs";
+import { FaCogs } from "react-icons/fa";
+import { ArrowRight } from "lucide-react";
+
+
+const CarItem = ({car}) => {
+  return (
+    <div className="bg-[#050B20] rounded-lg shadow-md w-full text-white">
+      <Image
+        src={car.images[0]}
+        alt={`${car.make} ${car.model}`}
+        width={400}
+        height={160}
+        className="w-full h-40 object-cover rounded-t-lg"
+      />
+
+      <div className="p-4">
+        <h4 className="font-bold text-lg">
+          {car.make} {car.model}
+        </h4>
+        <p className="text-sm mt-1">{car.engineSize}</p>
+        <div className="flex items-center gap-4 mt-2 text-sm">
+          <div className="flex items-center gap-1">
+            <MdSpeed className="h-4 w-4" />
+            <span>{car.mileage} Miles</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <BsFuelPump className="h-4 w-4" />
+            <span>{car.type}</span>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <FaCogs className="h-4 w-4" />
+            <span>{car.transmission}</span>
+          </div>
+        </div>
+        <div className="flex justify-between items-center mt-4">
+          <p className="font-bold text-2xl">
+            {car.sellingPrice}{" "}
+            <span className="font-semibold text-sm">ETB</span>{" "}
+          </p>
+          <a
+            href={`/listings/${car.id}`}
+            className="flex items-center text-sm hover:underline"
+          >
+            View Details <ArrowRight className="ml-1 h-4 w-4" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarItem;
--- next-js-car-marketplace/src/components/MostSearched.jsx
+++ next-js-car-marketplace/src/components/MostSearched.jsx
@@ -1,9 +1,5 @@
-import { ArrowRight } from "lucide-react";
 import { Carlisting } from "../../configs/schema";
-import Image from "next/image";
-import { MdSpeed } from "react-icons/md";
-import { BsFuelPump } from "react-icons/bs";
-import { FaCogs } from "react-icons/fa";
+import CarItem from "./CarItem";
 import {
   Carousel,
   CarouselContent,
@@ -45,46 +41,7 @@ async function MostSearched() {
                 key={`${car.id}-${index}`}
                 className="basis-full md:basis-1/2 lg:basis-1/3 px-2"
               >
-                <div className="bg-[#050B20] rounded-lg shadow-md w-full text-white">
-                  <Image
-                    src={car.images[0]}
-                    alt={`${car.make} ${car.model}`}
-                    width={400}
-                    height={160}
-                    className="w-full h-40 object-cover rounded-t-lg"
-                  />
-
-                  <div className="p-4">
-                    <h4 className="font-bold text-lg">
-                      {car.make} {car.model}
-                    </h4>
-                    <p className="text-sm mt-1">{car.engineSize}</p>
-                    <div className="flex items-center gap-4 mt-2 text-sm">
-                      <div className="flex items-center gap-1">
-                        <MdSpeed className="h-4 w-4" />
-                        <span>{car.mileage} Miles</span>
-                      </div>
-                      <div className="flex items-center gap-1">
-                        <BsFuelPump className="h-4 w-4" />
-                        <span>{car.type}</span>
-                      </div>
-
-                      <div className="flex items-center gap-1">
-                        <FaCogs className="h-4 w-4" />
-                        <span>{car.transmission}</span>
-                      </div>
-                    </div>
-                    <div className="flex justify-between items-center mt-4">
-                      <p className="font-bold text-2xl">{car.sellingPrice} <span className="font-semibold text-sm">ETB</span> </p>
-                      <a
-                        href={`/listings/${car.id}`}
-                        className="flex items-center text-sm hover:underline"
-                      >
-                        View Details <ArrowRight className="ml-1 h-4 w-4" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
+                <CarItem car={car} />
               </CarouselItem>
             ))}
           </CarouselContent>
